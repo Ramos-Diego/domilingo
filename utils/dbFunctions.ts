b80490403784ce7660b3e4edf4e26cf940bb0193
Ramos-Diego <RamosDiego@pm.me>
2020-12-06 17:10:43 -0500

    authentication fix

--- utils/dbFunctions.ts
+++ utils/dbFunctions.ts
@@ -1,26 +1,24 @@
 import { connectToDatabase } from './mongodb'
 
 // Todo: create better types for this function
-export const GetUserOrSaveNewUser = async (profile: { id?: string }) => {
-  // First check if there is a profile object
-  if (profile) {
-    const { db } = await connectToDatabase()
-    const user = await db.collection('users').findOne({ id: `${profile.id}` })
-    // Check if the user exists
-    if (user) {
-      // Return user in order to attach custom data to the JWT
-      return user
-    } else {
-      // Save the new user
-      await db.collection('users').insertOne({
-        ...profile,
-        // This portion can be whatever you want. It is a way to
-        // Persist information about the user in the database
-        dominilingo: {
-          // Convert all ids to string
-          role: 'user',
-        },
-      })
+export const GetUserOrSaveNewUser = async (profile: { id: string }) => {
+  const { db } = await connectToDatabase()
+  const user = await db.collection('users').findOne({ id: `${profile.id}` })
+  // Check if the user exists
+  if (user) {
+    // Return user in order to attach custom data to the JWT
+    return user
+  } else {
+    const newUser = {
+      ...profile,
+      // This portion can be whatever you want. It is a way to persist
+      // information about the new user in the database
+      domilingo: {
+        role: 'user',
+      },
     }
+    // Save the new user
+    await db.collection('users').insertOne(newUser)
+    return newUser
   }
 }
