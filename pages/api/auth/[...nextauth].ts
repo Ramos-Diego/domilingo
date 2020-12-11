28c3800f4146b74feabbd87951c77fdac55ccaf3
Ramos-Diego <RamosDiego@pm.me>
2020-12-11 13:20:10 -0500

    Authentication refactor

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,44 @@
+import { NextApiHandler } from 'next'
+import NextAuth, { InitOptions, User } from 'next-auth'
+import Providers from 'next-auth/providers'
+import { SessionBase } from 'next-auth/_utils'
+import { DomilingoUser, SessionUser } from '../../../lib/data-types'
+import { GetUserOrSaveNewUser } from '../../../utils/dbFunctions'
+
+const options: InitOptions & { jwt: { signingKey?: string } } = {
+  providers: [
+    Providers.Google({
+      clientId: process.env.NEXTAUTH_GOOGLE_ID || '',
+      clientSecret: process.env.NEXTAUTH_GOOGLE_SECRET || '',
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+    signingKey: process.env.NEXTAUTH_JWT_SIGNING_PRIVATE_KEY,
+  },
+  callbacks: {
+    jwt: async (token, user, account, profile, isNewUser) => {
+      if (user) {
+        const userFromDB = await GetUserOrSaveNewUser(profile)
+
+        token.domilingo = {
+          id: userFromDB.id,
+          role: userFromDB.domilingo.role,
+        }
+      }
+      return Promise.resolve(token)
+    },
+    session: async (
+      session: SessionBase & { user: SessionUser },
+      user: User & { domilingo: DomilingoUser }
+    ) => {
+      session.user.domilingo = user.domilingo
+      return Promise.resolve(session)
+    },
+  },
+}
+
+const Auth: NextApiHandler = (req, res) => NextAuth(req, res, options)
+
+export default Auth
